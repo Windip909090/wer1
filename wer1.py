fad47628d6c036d63c1b5e58ee3f23b4ac0c47a4
Windip909090 <81529326+Windip909090@users.noreply.github.com>
2021-03-29 02:23:33 +0700

    Update wer1.py

--- wer1.py
+++ wer1.py
@@ -817,14 +817,6 @@ def generate(text):
 					results.append("rahasia")
 
 					results.append("102030")
-                   
-                    results.append("Doraemon")
-                    
-                    results.append("anjing")
-                    
-                    results.append("bismillah")
-                    
-                    results.append("Kontol")
 	return results
 
 def methode():
